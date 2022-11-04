b13d59f660d29234efe3879a036856bf79562637
Valeria <soltanvaleria01@gmail.com>
2022-11-04 10:10:02 +0200

    Implement spring app

---
+++ src/main/java/com/aditya/demo/model/request/CreateRequest.java
@@ -0,0 +1,10 @@
+package com.aditya.demo.model.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CreateRequest {
+    private String name;
+}
